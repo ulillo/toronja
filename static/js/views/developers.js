d65ee460fce6b629f433a64fa60e10d2d0dc42e1
Jean Chassoul <chassoul@gmail.com>
2015-06-06 11:56:24 -0600

    ..

--- static/js/views/developers.js
+++ static/js/views/developers.js
@@ -24,5 +24,6 @@ fun.views.developers = Backbone.View.extend({
         var template = _.template(fun.utils.getTemplate(fun.conf.templates.developers));
 
         this.$el.html(template);
+        this.$el.show();
     }
 });
