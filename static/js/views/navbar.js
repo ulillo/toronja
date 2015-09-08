bf25d713ca1b6f52cd64f027a6c94e1b982fdaa9
Jean Chassoul <chassoul@gmail.com>
2015-09-08 12:39:05 -0600

    now with trim

--- static/js/views/navbar.js
+++ static/js/views/navbar.js
@@ -58,7 +58,7 @@ fun.views.navbar = Backbone.View.extend({
             this.$('#nav-new-org').removeClass('show').addClass('hide');
             this.$('#nav-new-member').removeClass('hide').addClass('show');
             this.$('#nav-new-team').removeClass('hide').addClass('show');
-        } else if (context === ' System Admin') {
+        } else if (context.trim() === 'System Admin') {
             console.log('remove or wut');
             this.$('#nav-new-account').removeClass('hide').addClass('show');
             this.$('#nav-new-gateway').removeClass('hide').addClass('show');
