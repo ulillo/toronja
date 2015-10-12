7395d5cf7f861e8407b78da4b088690ea4055f3b
Jean Chassoul <chassoul@gmail.com>
2015-10-11 21:20:47 -0600

    sin

--- static/js/seed/fun.utils.js
+++ static/js/seed/fun.utils.js
@@ -306,7 +306,7 @@ var round = fun.utils.round;
 
 var Theme = function () {
     
-    var chartColors, validationRules = getValidationRules ();
+    var chartColors;
     
     // Black & Orange
     //chartColors = ["#FF9900", "#333", "#777", "#BBB", "#555", "#999", "#CCC"];
@@ -320,58 +320,7 @@ var Theme = function () {
     // Mean Green
     chartColors = ['#5F9B43', '#DB7D1F', '#BA4139', '#777','#555','#999','#bbb','#ccc','#eee'];
     
-    return { init: init, chartColors: chartColors, validationRules: validationRules };
-    
-    function init () {
-        enhancedAccordion ();
-        
-        if ($.fn.lightbox) {
-            $('.ui-lightbox').lightbox();
-        }
-        
-        if ($.fn.cirque) {
-            $('.ui-cirque').cirque ({  });
-        }
-    
-        $('#wrapper').append ('<div class="push"></div>');
-    }
-    
-    function enhancedAccordion () {
-        $('.accordion').on('show', function (e) {
-             $(e.target).prev('.accordion-heading').parent ().addClass('open');
-        });
-    
-        $('.accordion').on('hide', function (e) {
-            $(this).find('.accordion-toggle').not($(e.target)).parents ('.accordion-group').removeClass('open');
-        });
-        
-        $('.accordion').each (function () {            
-            $(this).find ('.accordion-body.in').parent ().addClass ('open');
-        });
-    }
-    
-    function getValidationRules () {
-        var custom = {
-            focusCleanup: false,
-            
-            wrapper: 'div',
-            errorElement: 'span',
-            
-            highlight: function(element) {
-                $(element).parents ('.control-group').removeClass ('success').addClass('error');
-            },
-            success: function(element) {
-                $(element).parents ('.control-group').removeClass ('error').addClass('success');
-                $(element).parents ('.controls:not(:has(.clean))').find ('div:last').before ('<div class="clean"></div>');
-            },
-            errorPlacement: function(error, element) {
-                error.appendTo(element.parents ('.controls'));
-            }
-            
-        };
-        
-        return custom;
-    }
+    return { chartColors: chartColors };
     
 }();
 
