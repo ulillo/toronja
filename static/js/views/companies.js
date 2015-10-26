f816541e09718ce5a14da7ad723d31dcf2996e54
Jean Chassoul <chassoul@gmail.com>
2015-10-26 14:50:12 -0600

    dale

--- static/js/views/companies.js
+++ static/js/views/companies.js
@@ -534,6 +534,10 @@ fun.views.companies = Backbone.View.extend({
                 //account = response.get('account');
                 //email = response.get('email');
 
+                var test = response.get('status');
+
+                console.log(test);
+
                 companyUuid.html(response.get('uuid'));
 
                 account.html(response.get('account'));
@@ -542,7 +546,7 @@ fun.views.companies = Backbone.View.extend({
 
                 companyName.html(response.get('company_name'));
 
-                companyStatus.html(response.get('status', 'voz'));
+                companyStatus.html();
 
                 streetAddress.html(response.get('street_address'));
                 cityTown.html(response.get('city_town'));
