2bbc566cd61a833e9fa2564cc1cd902a236e5d14
Jean Chassoul <chassoul@gmail.com>
2015-11-05 19:38:58 -0600

    no

---
+++ static/js/views/workspace.js
@@ -0,0 +1,28 @@
+fun.views.workspace = Backbone.View.extend({
+
+    /*
+    * Bind the event functions to the different HTML elements
+    */
+    events : {
+
+    },
+    
+    /*
+    * Class constructor
+    */
+    initialize : function(options) {
+        fun.containers.workspace = this.$el;
+    },
+    
+    /*
+    * Render the workspace view
+    */
+    render : function(){
+        if (!this.$el.html()){
+            var template = _.template(fun.utils.getTemplate(fun.conf.templates.workspace));
+            this.$el.html(template);
+        }
+        this.$el.removeClass("hide").addClass("show");
+    }
+
+});
